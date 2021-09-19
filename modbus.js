e6f620671b12fa7be92459cfe4d44cfaa59b2218
tannedsnowman <52172321+tannedsnowman@users.noreply.github.com>
2021-09-19 13:42:46 +0100

    Update

--- modbus.js
+++ modbus.js
@@ -2,25 +2,12 @@ var ModbusRTU = require("modbus-serial")
 var client = new ModbusRTU()
 
 // open connection to a serial port
-try {
-    client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600 })
-} catch (e) {
-    console.log(e)
-    client.conUnBectRTUBuffered("/dev/ttyUSB1", { baudRate: 9600 })
-}
+client.connectRTUBuffered("/dev/ttyUSB0", { baudRate: 9600 })
 
 client.setID(1)
 
-// client.setTimeout(500)
-
-const read = async () => {
-  try {
-    console.log("here")
-      const val = await client.readInputRegisters(33000, 100)
-    console.log(val)
-  } catch (e) {
-    console.log(e)
-  }
-}
-
-read()
+setInterval(function() {
+  client.readHoldingRegisters(33000, 100, function(err, data) {
+      console.log(err, data.data);
+  });
+}, 1000);
